7f902ee97337b230a642c3f95bb7383ad0c53133
fesko32 <fesko_il@icloud.com>
2025-04-11 20:37:09 +0300

    Подготавливает проект и добавляет стили для hero

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,18 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+Swiper.use([Pagination, Navigation]);
+
+const swiper = new Swiper('.swiper', {
+  pagination: {
+
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<button class="${className} custom-bullet" type="button"></button>`;
+    },
+  },
+});
