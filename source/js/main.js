2c0b43b933b59346194c3cbe27f3783c3f6e5478
fesko32 <fesko_il@icloud.com>
2025-04-23 22:44:28 +0300

    Добавляет слайдер в блоке tours

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,10 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 
 Swiper.use([Pagination, Navigation]);
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.hero__swiper.swiper', {
   pagination: {
 
     el: ".swiper-pagination",
@@ -15,4 +13,33 @@ const swiper = new Swiper('.swiper', {
       return `<button class="${className} custom-bullet" type="button"></button>`;
     },
   },
+});
+
+const swiper2 = new Swiper('.tours__swiper.swiper', {
+  modules: [Navigation],
+  grabCursor: true,
+  loop: false,
+  preloadImages: false,
+  initialSlide: 0,
+  lazy: true,
+  navigation: {
+    prevEl: '.tours__button.arrow-button--prev',
+    nextEl: '.tours__button.arrow-button--next',
+  },
+
+  breakpoints: {
+    336: {
+      slidesPerView: 1
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18
+    },
+
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
 });
